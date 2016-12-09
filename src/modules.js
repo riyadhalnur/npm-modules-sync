991bae3f0fc2aa66b8e258b32b649fcc4b95c713
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2016-12-10 01:34:28 +0600

    Module to install and get list of NPM packages

---
+++ src/modules.js
@@ -0,0 +1,41 @@
+// @flow
+'use strict';
+
+const execa = require('execa');
+
+/**
+ * Returns a list of all globally installed NPM packages
+ * @public
+ * @returns {Promise.<Object|string>} Returns installed packages/stderr on success/failure.
+ */
+const getModules = (): Promise<Object | string> => {
+  return new Promise((resolve, reject) => {
+    execa('npm', ['ls', '-g', '--depth=0', '--json=true']).then(result => {
+      resolve(JSON.parse(result.stdout));
+    }).catch(err => {
+      reject(err.stderr);
+    });
+  });
+};
+
+/**
+ * Installs a list of NPM packages in one go globally
+ * @public
+ * @param {Object} packages - List of packages to install
+ * @returns {Promise.<string>} Returns string on success/error
+ */
+const installModules = (packages: Object): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    let args = ['install', '-g'].concat(Object.keys(packages.dependencies));
+    execa('npm', args).then(result => {
+      resolve(result.stdout);
+    }).catch(err => {
+      reject(err.stderr);
+    });
+  });
+};
+
+module.exports = {
+  get: getModules,
+  install: installModules
+};
