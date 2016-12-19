70e489f0870d13c23e2f2f6fee57335749904261
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2016-12-19 15:53:44 +0600

    Rename folder inside tests/

---
+++ tests/helpers/fixtures.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const token = '12345';
+const gistId = '123456789';
+const gistContent = { hey: true, hello: 'world' };
+
+module.exports = {
+  token: token,
+  gistId: gistId,
+  gistContent: gistContent
+};
