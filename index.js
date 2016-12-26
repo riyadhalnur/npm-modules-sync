cb53a80495fe6fa8104de6d9deb81b786a55a965
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2016-12-27 01:11:27 +0600

    Main entry file for CLI

---
+++ index.js
@@ -0,0 +1,34 @@
+/* eslint no-console: 0, max-len: 1 */
+'use strict';
+
+const emoji = require('node-emoji');
+const chalk = require('chalk');
+
+const store = require('./build/store');
+const tasks = require('./build/tasks');
+
+module.exports = (opts, flags) => {
+  switch (opts) {
+  case 'upload':
+    tasks.upload.run({ token: store.get('token'), gistId: store.get('gistId') });
+    break;
+  case 'download':
+    tasks.download.run({ token: store.get('token'), gistId: store.get('gistId') });
+    break;
+  case 'init':
+    if (flags.token && !flags.id) {
+      tasks.init.run({ token: flags.token }).then(ctx => {
+        console.log(`Your Gist ID is ${chalk.yellow(ctx.gistId)}. ${chalk.red.bold('Please')} note it down.
+        You will need this when configuring machines to sync to.`);
+      }).catch(err => {
+        console.log(err);
+      });
+    } else {
+      tasks.initSlave.run({ token: flags.token, gistId: flags.id });
+    }
+
+    break;
+  default:
+    break;
+  }
+};
